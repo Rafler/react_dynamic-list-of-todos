44473caf39d2af89c633eb450cced6b4bf1a4afa
Rafler <amuronyk@gmail.com>
2019-09-24 11:00:15 +0300

    Updates

---
+++ precache-manifest.9740b0ade180208d9faa1c17e581cd8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86acabd9cff183bd3715d886bc7d7b3d",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "3a9aab48b3a54a629de3",
+    "url": "/react_dynamic-list-of-todos/static/css/main.7f4b24b3.chunk.css"
+  },
+  {
+    "revision": "a98bb21db1e89d8a2954",
+    "url": "/react_dynamic-list-of-todos/static/js/2.d99334fc.chunk.js"
+  },
+  {
+    "revision": "3a9aab48b3a54a629de3",
+    "url": "/react_dynamic-list-of-todos/static/js/main.a92a3e7f.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
